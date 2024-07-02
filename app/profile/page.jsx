7e1e59857ac36c1c4da1365daf7d2ec87a4efb43
Harshit <harshit70803hs@gmail.com>
2024-07-02 21:56:47 +0530

    typo

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,8 +27,8 @@ const MyProfile = () => {
 
     
     const handleEdit = (post)=>{
-        router.push(`update-prompt?id=${post._id}`)
-    }
+        router.push(`/update-prompt?id=${post._id}`);
+    };
 
     const handleDelete = async (post)=>{
         const hasConfirmed = confirm(
