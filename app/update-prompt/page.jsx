65288fb8ea34aebb2983749fee43f4d140245f56
Harshit <harshit70803hs@gmail.com>
2024-07-02 19:54:20 +0530

    typo

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,42 +1,37 @@
-'use client'
+"use client";
 
-import { useState,useEffect } from 'react'
-import { useRouter,useSearchParams } from 'next/navigation'
+import { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
-import Form from '@components/Form'
+import Form from '@components/Form';
 
 const UpdatePrompt = () => {
-    // const { data: session } = useSession();
-
     const router = useRouter();
     const searchParams = useSearchParams();
     const promptId = searchParams.get("id");
-    
-    const [submitting, setSubmitting] = useState(false);
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: ''
-    });
 
-    useEffect(()=>{
-        const getPromptDetails = async()=>{
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({ prompt: "", tag: "", });
+
+    useEffect(() => {
+        const getPromptDetails = async () => {
             const response = await fetch(`api/prompt/${promptId}`);
             const data = await response.json();
 
             setPost({
-                prompt:data.prompt,
-                tag:data.tag,
+                prompt: data.prompt,
+                tag: data.tag,
             });
         };
 
-        if(promptId) getPromptDetails();
-    },[promptId]);
+        if (promptId) getPromptDetails();
+    }, [promptId]);
 
     const updatePrompt = async (e) => {
         e.preventDefault();
         setSubmitting(true);
 
-        if(!promptId) return alert('prompt ID not Found');
+        if (!promptId) return alert('prompt ID not Found');
 
         try {
             const response = await fetch(`/api/prompt/${promptId}`, {
@@ -56,7 +51,7 @@ const UpdatePrompt = () => {
             setSubmitting(false);
         }
     };
-    
+
     return (
         <Form
             type="Edit"
