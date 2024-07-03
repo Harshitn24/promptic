86331688fdb9f023f3c212e86e08c4b414a2ded9
Harshit <harshit70803hs@gmail.com>
2024-07-03 19:11:24 +0530

    typo

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -26,8 +26,9 @@ const MyProfile = () => {
         },[]);
 
     
+        
     const handleEdit = (post)=>{
-        router.push(`/update-prompt?id=${post._id}`);
+        // router.push(`/update-prompt?id=${post._id}`);
     };
 
     const handleDelete = async (post)=>{
